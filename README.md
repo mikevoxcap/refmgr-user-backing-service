85d13bcb6227d7ffc30c88d7c5e2ae8e0da32078
Michael Hoffman <mike@michaelhoffmaninc.com>
2016-08-13 13:42:34 -0500

    Committing initial codebase

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # refmgr-user-backing-service
-Backing service for authorization and authentication
+Backing service for authorization and authentication of users for the referral management system. 
---
+++ src/main/java/com/michaelhoffmaninc/refmgr/user/ReferralManagerUserBackingServiceApplication.java
@@ -0,0 +1,18 @@
+package com.michaelhoffmaninc.refmgr.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application for the user backing service
+ * 
+ * @author Michael Hoffman
+ *
+ */
+@SpringBootApplication
+public class ReferralManagerUserBackingServiceApplication {
+
+   public static void main(String[] args) {
+      SpringApplication.run(ReferralManagerUserBackingServiceApplication.class, args);
+   }
+}
